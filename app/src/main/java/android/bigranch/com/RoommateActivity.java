29f0f2d6d67023424d2ed3e9ffb02095b8348151
David Sharratt <david.sharratt@aggiemail.usu.edu>
2019-08-27 23:22:39 -0600

    Initial commit with the first Model class and member variables added.

---
+++ app/src/main/java/android/bigranch/com/RoommateActivity.java
@@ -0,0 +1,14 @@
+package android.bigranch.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoommateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
