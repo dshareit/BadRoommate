000e734b3c3b24f0d67671c644cc2ccaf523f267
David Sharratt <david.sharratt@aggiemail.usu.edu>
2019-09-11 02:19:06 -0600

    RecyclerView, ViewHolder, and layouts for task list and task item made.

--- app/src/main/java/android/bigranch/com/TaskListFragment.java
+++ app/src/main/java/android/bigranch/com/TaskListFragment.java
@@ -1,7 +1,70 @@
 package android.bigranch.com;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class TaskListFragment extends Fragment {
+    private RecyclerView mTaskRecyclerView;
+    private TaskAdapter mAdapter;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+        mTaskRecyclerView = (RecyclerView) view.findViewById(R.id.task_recycler_view);
+        mTaskRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+        return  view;
+
+    }
+
+    private void updateUI() {
+        TaskLab taskLab = TaskLab.get(getActivity());
+        List<Task> tasks = taskLab.getTasks();
+
+        mAdapter = new TaskAdapter(tasks);
+        mTaskRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class TaskHolder extends RecyclerView.ViewHolder {
+        public TaskHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_task, parent, false));
+        }
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder> {
+        private List<Task>  mTasks;
+
+        public TaskAdapter(List<Task> tasks) {
+            mTasks = tasks;
+        }
+
+        @NonNull
+        @Override
+        public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            return new TaskHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTasks.size();
+        }
+    }
 }
